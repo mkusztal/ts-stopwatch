1c36fa28ab5a978f8433dfac91f0207c40ceb19f
sicArchitect <maciej.kusztal1995@gmail.com>
2023-05-11 22:14:41 +0300

    Complementary functions

--- src/Stopwatch.ts
+++ src/Stopwatch.ts
@@ -1,83 +1,86 @@
+interface StopwatchDom {
+  currentTime: HTMLDivElement;
+  startBtn: HTMLButtonElement;
+  stopBtn: HTMLButtonElement;
+  resetBtn: HTMLButtonElement;
+  [x: string]: HTMLElement;
+}
+
 abstract class Stopwatch {
+  protected currentTime: number = 0;
+  protected timer: number | null = null;
+  protected dom = <StopwatchDom>{};
 
-  currentTime = 0
-  timer = null
-  dom = {}
-
-  constructor(element) {
-    this.getElements(element)
-    this.initActions()
-    this.renderTime()
+  constructor(element: HTMLDivElement) {
+    this.getElements(element);
+    this.initActions();
+    this.renderTime();
   }
 
-  getElements(element) {
-    /*
-    Funkcja ta powinna przyjąć jako argument referencję do elementu DOM, w którym znajduje się
-    cała struktura stopera. Następnie powinna przygotować referencję do obecnych w tym elemencie dzieci.
-    
-    Konkretnie do: 
-    – stopwatch__current-time, 
-    – wszystkich buttonów stopwatch__actions
-
-    Wszystkie referencje dla czytelności przechowuj w obiekcie this.dom.
-    */
+  private getElements(element: HTMLDivElement): void {
+    this.dom = {
+      currentTime: <HTMLDivElement>(
+        element.querySelector(".stopwatch__current-time")
+      ),
+      startBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__start-btn")
+      ),
+      stopBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__stop-btn")
+      ),
+      resetBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__reset-btn")
+      ),
+    };
   }
 
-  initActions() {
-    /*
-    Funkcja ta powinna nadać buttonom z buttonów stopwatch__actions odpowiednie nasłuchiwacze na event click. 
-    Kliknięcie na każdy z buttonów powinno uruchamiać odpowiednie funkcje.
-
-    Start -> start()
-    Stop -> stop()
-    Reset -> reset()
-
-    Aby dostać się do tych elementów, wykorzystaj referencję przygotowane wcześniej w funkcji this.getElements.
-    */
+  private initActions(): void {
+    this.dom.startBtn.addEventListener("click", () => this.start());
+    this.dom.stopBtn.addEventListener("click", () => this.stop());
+    this.dom.resetBtn.addEventListener("click", () => this.reset());
   }
 
-  formatTime(time) {
-    /*
-    Funkcja ta powinna przyjmować czas w milisekundach a następnie zwracać go w formacie mm:ss:ms (np. 02:23:12).
-    */
-  } 
+  protected formatTime(time: number): string {
+    const pad = (num: number): string =>
+      num < 10 ? `0${num}` : num.toString();
 
-  renderTime() {
-    /*
-    Funkcja ta powinna renderować w stopwatch__current-time zawartość obiektu this.currentTime.
-    Oczywiście wcześniej należy sformatować czas przy użyciu funkcji this.formatTime. 
-    */
+    const mm = Math.floor(time / 6000);
+    const ss = Math.floor(time - (mm * 6000) / 1000);
+    const ms = time - mm * 6000 - ss * 1000;
+
+    return `${pad(mm)}:${pad(ss)}:${pad(ms).substr(0, 2)}`;
   }
 
-  start() {
+  protected renderTime(): void {
+    this.dom.currentTime.innerHTML = this.formatTime(this.currentTime);
+  }
+
+  protected start(): void {
     /*
     Funkcja ta powinna wystartować interwał, który będzie wykonywał się co milisekundę.
     Powinien on każdorazowo włączać funkcję this.step
 
     Dla wygody przypisz ten interwał do this.timer
     */
+    this.timer = window.setInterval(() => {
+      this.step();
+    }, 1000);
   }
 
-  step() {
-    /*
-    Funkcja ta powinna zwiększać liczbę sekund w this.currentTime o jeden, a następnie uruchamiać metodę
-    renderującą aktualny czas w HTML-u (this.renderTime).
-    */
+  protected step(): void {
+    this.currentTime += 1;
+    this.renderTime();
   }
 
-  stop() {
-    /* 
-    Funkcja ta powinna zatrzymywać interval przypisany do this.timer.
-    */
+  protected stop(): void {
+    this.timer = window.setInterval(() => {
+      this.currentTime;
+    }, this.currentTime);
   }
 
-  reset() {
-    /*
-    Ta funkcja powinna resetować czas zapisany w this.currentTime, a więć zmieniać jego wartość na zero.
-    Naturalnie powinno to wiązać się również z przerenderowaniem HTML-a (this.renderTime).
-    */
+  protected reset(): void {
+    clearInterval(this.currentTime);
   }
-
 }
 
-export default Stopwatch
+export default Stopwatch;
--- src/StopwatchWithResults.ts
+++ src/StopwatchWithResults.ts
@@ -1,50 +1,75 @@
-import Stopwatch from './Stopwatch.js'
+import Stopwatch from "./Stopwatch.js";
 
 class StopwatchWithResults extends Stopwatch {
-  
-  results = []
+  public results: number[] | null[] = [];
 
-  constructor(element) {
-    super(element)
-    this.prepareElements(element)
-    this.prepareActions()
+  constructor(element: HTMLDivElement) {
+    super(element);
+    this.prepareElements(element);
+    this.prepareActions();
   }
 
-  prepareElements(element) {
-    this.dom.resultsList = element.querySelector('.stopwatch__results')
-    this.dom.addToListBtn = element.querySelector('.stopwatch__start-add-to-list')
-    this.dom.resetListBtn = element.querySelector('.stopwatch__start-reset-list')
+  prepareElements(element: HTMLDivElement) {
+    this.dom = {
+      resultsList: <HTMLDivElement>element.querySelector(".stopwatch__results"),
+      addToListBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__start-add-to-list")
+      ),
+      resetListBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__start-reset-list")
+      ),
+      currentTime: <HTMLDivElement>(
+        element.querySelector(".stopwatch__current-time")
+      ),
+      startBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__start-btn")
+      ),
+      stopBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__stop-btn")
+      ),
+      resetBtn: <HTMLButtonElement>(
+        element.querySelector(".stopwatch__button stopwatch__reset-btn")
+      ),
+    };
   }
 
-  prepareActions() {
+  private prepareActions(): void {
     /*
     Funkcja ta powinna dodawać nasłuchwiacze do buttonów this.dom.addToListBtn oraz this.dom.resetListBtn.
     Pierwszy powinien po kliknięciu uruchamiać metodę this.addToList, a druga this.resetList.
     */
+
+    this.dom.addToListBtn.addEventListener("click", () => this.addToList());
+    this.dom.removeListBtn.addEventListener("click", () => this.resetList());
   }
 
-  renderList() {
+  protected renderList(): void {
     /*
     Funkcja ta powinna czyścić zawartość this.dom.resultsList, a następnie renderować w niej nowe elementy li
     na podstawie zawartości tablicy this.results. Każdy jej element powinien być renderowany bez żadnych zmian.
 
     np. <li>00:12:00</li>
     */
+    this.dom.resultsList.innerHTML = "";
+    this.dom.resultsList.innerHTML = this.results.join(" ");
   }
 
-  addToList() {
+  private addToList(): void {
     /*
     Funkcja ta powinna pobierać aktualny czas z this.currentTime, formatować go i w takiej postaci zapisywać do tablicy this.results.
     Następnie powinna renderować aktualną listę na stronie (this.renderList).
     */
+    this.formatTime(this.currentTime);
+    this.renderList();
   }
 
-  resetList() {
+  private resetList(): void {
     /*
     Funkcja ta powinna czyścić tablicę this.results oraz zawartość this.dom.resultsList
     */
+    this.dom.resultsList.innerHTML = "";
+    this.results.splice(0);
   }
-
 }
 
-export default StopwatchWithResults
+export default StopwatchWithResults;
